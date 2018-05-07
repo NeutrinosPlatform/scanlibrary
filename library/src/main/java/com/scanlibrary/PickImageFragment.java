1f66dafce15aa0d0ffdd821d5bd7c8c8fb0a8a57
ChrisTomAlx <christythomasalex@gmail.com>
2018-05-07 15:52:59 +0530

    changed provider in pickimagefragment again

--- library/src/main/java/com/scanlibrary/PickImageFragment.java
+++ library/src/main/java/com/scanlibrary/PickImageFragment.java
@@ -276,9 +276,9 @@ public class PickImageFragment extends Fragment implements  OnDialogButtonClickL
         File file = createImageFile();
         boolean isDirectoryCreated = file.getParentFile().mkdirs();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-             String authority =  com.scanlibrary.BuildConfig.APPLICATION_ID + ".com.scanlibrary.provider"; // As defined in Manifest
+            // String authority =   + ".com.scanlibrary.provider"; // As defined in Manifest
             Uri tempFileUri = FileProvider.getUriForFile(getActivity().getApplicationContext(),
-                    authority, // As defined in Manifest
+                    com.scanlibrary.BuildConfig.APPLICATION_ID, // As defined in Manifest
                     file);
             cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, tempFileUri);
         } else {
