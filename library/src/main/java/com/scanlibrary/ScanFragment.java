b3eb5bf3edfe80b4a0b5e95cb3437048ac96796a
grimaldistudio <domenico@grimaldistudio.com>
2019-05-08 18:36:45 +0200

    Update ScanFragment.java

--- library/src/main/java/com/scanlibrary/ScanFragment.java
+++ library/src/main/java/com/scanlibrary/ScanFragment.java
@@ -179,8 +179,8 @@ public class ScanFragment extends Fragment {
     }
 
     private Bitmap getScannedBitmap(Bitmap original, Map<Integer, PointF> points) {
-        int width = original.getWidth();
-        int height = original.getHeight();
+        int width = original.getWidth() / 2;
+        int height = original.getHeight() / 2;
         float xRatio = (float) original.getWidth() / sourceImageView.getWidth();
         float yRatio = (float) original.getHeight() / sourceImageView.getHeight();
 
