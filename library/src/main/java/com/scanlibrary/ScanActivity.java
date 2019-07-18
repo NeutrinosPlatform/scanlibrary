1eb25e99957a4a8bedde5c2d4c12fc6d254d7fdd
Christy Thomas Alex <34361280+ChrisTomAlx@users.noreply.github.com>
2019-07-18 12:42:01 +0530

    Update ScanActivity.java

--- library/src/main/java/com/scanlibrary/ScanActivity.java
+++ library/src/main/java/com/scanlibrary/ScanActivity.java
@@ -7,6 +7,7 @@ import android.content.ComponentCallbacks2;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
 /**
  * Created by jhansi on 28/03/15.
@@ -24,6 +25,9 @@ public class ScanActivity extends Activity implements IScanner, ComponentCallbac
         PickImageFragment fragment = new PickImageFragment();
         Bundle bundle = new Bundle();
         bundle.putInt(ScanConstants.OPEN_INTENT_PREFERENCE, getPreferenceContent());
+        Log.wtf("qualityScan", "Here we GOOOOOOOOOOO");
+        Log.wtf("qualityScan", String.valueOf(getIntent().getIntExtra("quality", 1)));
+        bundle.putInt("quality", getIntent().getIntExtra("quality", 1));
         fragment.setArguments(bundle);
         android.app.FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
