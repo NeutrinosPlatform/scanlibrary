43f45a8ebaf797a74fc005203a5e9c2771102d33
Christy Thomas Alex <34361280+ChrisTomAlx@users.noreply.github.com>
2020-04-12 14:13:34 +0530

    Update Utils.java

--- library/src/main/java/com/scanlibrary/Utils.java
+++ library/src/main/java/com/scanlibrary/Utils.java
@@ -4,16 +4,18 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.util.Log;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by jhansi on 05/04/15.
  */
 public class Utils {
-
+    static Date currentTime;
     private Utils() {
 
     }
@@ -21,8 +23,10 @@ public class Utils {
     public static Uri getUri(Context context, Bitmap bitmap) {
         ByteArrayOutputStream bytes = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        Log.wtf("PATH", "before insertImage");
         // String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), bitmap, "Title", null);
-        String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), bitmap, "Title" + " - " + Calendar.getInstance().getTime(), null);
+        String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), bitmap, "Title" + " - " + (currentTime = Calendar.getInstance().getTime()), null);
+        Log.wtf("PATH", path);
         return Uri.parse(path);
     }
 
